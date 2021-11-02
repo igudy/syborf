86b19cd5ec8426cddad23bd933fc5c7631fdaf40
Goodness Igunma <Goodnessigunma1@gmail.com>
2021-11-02 14:04:39 +0100

    Inital Commit

---
+++ index.php
@@ -0,0 +1,39 @@
+    <!--================Head Menu Area =================-->
+    <?php include("includes/head.php"); ?>
+    <!--================Head Menu Area =================-->
+
+    <body id="page-top">
+
+    <!--================Navigation Menu Area =================-->
+    <?php include("includes/navigation.php"); ?>
+    <!--================Navigation Menu Area =================-->
+
+    <!--================Header Menu Area =================-->
+    <?php include("includes/header.php"); ?>
+    <!--================Header Menu Area =================-->
+
+    <!--================Portfolio Area =================-->
+    <?php include("includes/portfolio.php"); ?>
+    <!--================Portfolio Area =================-->
+
+    <!--================About Area =================-->
+    <?php include("includes/about.php"); ?>
+    <!--================About Area =================-->
+
+    <!--================Contact Area =================-->
+    <?php include("includes/contact.php"); ?>
+    <!--================Contact Area =================-->
+
+    <!--================Footer Area =================-->
+    <?php include("includes/footer.php"); ?>
+    <!--================Footer Area =================-->
+
+    <!--================Copyright Area =================-->
+    <?php include("includes/copyright.php"); ?>
+    <!--================Copyright Area =================-->
+
+    <!--================PortfolioModal Area =================-->
+    <?php include("includes/portfolioModal.php"); ?>
+    <!--================PortfolioModal Area =================-->
+
+    </body>
